d19ac3e23ce7a92daaeb6245497b499ca96fb967
viaann <darkness14042@gmail.com>
2024-03-25 22:43:58 +0700

    add .gitignore

--- guessing_game/target/debug/deps/cfg_if-0f14f973694288b0.d
+++
@@ -1,7 +0,0 @@
-D:\Projects\learn-rust\guessing_game\target\debug\deps\libcfg_if-0f14f973694288b0.rmeta: C:\Users\viaann\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-D:\Projects\learn-rust\guessing_game\target\debug\deps\libcfg_if-0f14f973694288b0.rlib: C:\Users\viaann\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-D:\Projects\learn-rust\guessing_game\target\debug\deps\cfg_if-0f14f973694288b0.d: C:\Users\viaann\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
-
-C:\Users\viaann\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
--- guessing_game/target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\learn-rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
-
-D:\Projects\learn-rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
-
-src\main.rs:
